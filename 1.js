0563a79c8e4f5414a43d3df9e82090f75620590c
VisualOption <gunmetal90@gmail.com>
2022-07-02 20:55:14 +0300

    Homework JS1

---
+++ 1.js
@@ -0,0 +1,30 @@
+let user_name = "Ruslan"
+// let name2 = Ruslan
+// console.log(user_name)
+
+let age = 31
+// console.log(age)
+
+let person = user_name + age
+// console.log(person)
+
+
+let user_age = 30
+let work_period = 10
+
+let total = user_age + work_period
+// console.log("Total time =", total, typeof(total))
+
+let morning = false
+// console.log("Morning is ", morning, typeof(morning))
+
+
+let apples = 10
+let cherry = 20
+
+let compare = apples < cherry
+console.log(compare)
+
+if (compare){
+    console.log("29 line performed")
+}
