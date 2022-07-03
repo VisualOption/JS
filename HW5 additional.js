a407475905d3e069d794fa0d7695d9a9fd4d2d80
VisualOption <38600007+VisualOption@users.noreply.github.com>
2022-07-03 11:42:13 +0300

    Update HW5 additional.js

--- HW5 additional.js
+++ HW5 additional.js
@@ -65,6 +65,31 @@
 // task1()
 
 
+const age2 = 18
+const age3 = 60
+
+const checkAge = function(age){
+    if (age && !isNaN(age)){
+        if (age <age2){
+            console.log("You don’t have access cause your age is " + age_1 + " It’s less then ")
+        }
+        else if (age >= age2 && age < age3){
+            console.log("Welcome  !")
+        }
+        else if (age > age3){
+            console.log("Keep calm and look Culture channel")
+        }
+        else {
+            console.log("Technical work")
+        }
+    }
+    else console.log("Not an integer value")
+}
+
+let a = prompt("Enter your age")
+
+checkAge(a)
+
 // function task1(age_1 = prompt('Enter 1st age', 17)){
 //     if (typeof(age_1 && age_2 && age_3) !== 'number'){
 //         let new_age_1 = parseInt(age_1)
